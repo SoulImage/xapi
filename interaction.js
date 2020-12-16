ab7f8eda0a07b1c928bc7232dd93b806bd81746a
SoulImage <75799019+SoulImage@users.noreply.github.com>
2020-12-16 12:03:42 -0500

    Update interaction.js

--- interaction.js
+++ interaction.js
@@ -1,42 +1,53 @@
-//Variables
-var name = "James Wentworth";
-var email = "table1235@gmail.com"
+// Variables
+var name = "Sammy McGee";
+var email = "jeffbatt@gmail.com"
 
-
+// Page Load Function
 function pageLoaded(){
-    // Code goes here
+	// Connecting to LRS
+	var conf = {
+	  "endpoint" : "https://cloud.scorm.com/tc/CJ0NK8AJKV/sandbox/",
+	  "auth" : "Basic " + toBase64('jeffbatt@gmail.com:DevLearn17'),
+	};
+	ADL.XAPIWrapper.changeConfig(conf);
 }
 
-function sendOverStatement(){
-	var statement = {
-		"actor": {
-			"mbox": "mailto:" + email,
-			"name": name,
-			"objectType": "Agent"
-		},
-		"Verb": {
-			"id": "http://adlnet.gov/expapi/verbs/interacted",
-			"display": {"en-US": "interacted"}
-		},
-		"Object": {
-			"id": "http:testing.net"
-			"definition": {
-				"name": {"en-US": "Simple button example"},
-				"description": {"en-US": "Simple button example xAPI Button"}
-			},
-			"objectType": "Activity"
-		}
+// Button click
+function sendStatement(){
+	var statement = {  
+	    "actor": {  
+	        "mbox": "mailto:"+email,  
+	        "name": name,  
+	        "objectType": "Agent"  
+	    },  
+	    "verb": {  
+	        "id": "http://adlnet.gov/expapi/verbs/interacted",  
+	        "display": {"en-US": "interacted"}  
+	    },  
+	    "object": {  
+	        "id": "http://learningdojo.net/xapi/simple_button",  
+	        "definition": {  
+	            "name": {"en-US": "Simple button example"},  
+	            "description": {"en-US": "Simple button example xAPI Button"}  
+	        },  
+	        "objectType": "Activity"  
+	    }  
+	};
 
-	}
-	alert("Statement has been sent over.")
-	ADL.XAPIWRAPPER.sendStatement(statement);
+	ADL.XAPIWrapper.sendStatement(statement); 
+
+	// Alert message
+	alert('Statement has been sent!')
+
 }
 
-//On Blur Events
-function saveName() {
+// On Blur Events
+function saveName(){
 	name = document.getElementById('nameEntered').value;
+	console.log(name);
 }
 
-function saveEmail() {
+function saveEmail(){
 	email = document.getElementById('userEmail').value;
+	console.log(email)
 }
